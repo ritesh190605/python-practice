002ee8b33c6db22f6e8277c0f376b268326ef951
ritesh190605 <riteshblackdotsco@gmail.com>
2026-03-29 23:59:02 +0530

    datatypes

---
+++ conditional.py
@@ -0,0 +1,50 @@
+#basic if and else codition
+x=10
+if x>5 :
+    print("True")
+else:
+    print("False")
+
+
+#basic if elif statement for giving result my entering marks
+marks=85
+if marks>=60 :
+    print("pass")
+elif marks>=80 :
+    print("good")
+else:
+    print("fail")
+
+
+ # some more sample examples of nested if - else satements   
+ 
+ #example 1:
+x=10
+if x>5:
+    if x>8:
+        print("a")
+    else:
+        print("b")
+else:
+    print("c")
+
+#example 2:
+x=8
+if x>5:
+    if x>8:
+        print("a")
+    else:
+        print("b")
+else:
+    print("c")
+
+#example 3:
+x=5
+if x>5:
+    if x>8:
+        print("a")
+    else:
+        print("b")
+else:
+    print("c")
+
---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a =4
+b=3.0
+c="stri "
+d= True
+print(type(a))
+print(type(b)) 
+print(type(c)) 
+print(type(d))
