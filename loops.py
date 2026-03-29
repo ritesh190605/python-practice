fb14e5fd980eebbee05673d2f82900cb24e0679b
ritesh190605 <riteshblackdotsco@gmail.com>
2026-03-30 00:09:12 +0530

    LOOPS

---
+++ loops.py
@@ -0,0 +1,35 @@
+# printing the letters of banana
+for i in('banana'):
+    print(i)
+
+#printing numbers till 3 using while loop
+i=1
+while i<=3:
+    print(i)
+    i+=1
+
+#making infinite loop using while    
+i=1
+while i<=5:
+    print(i)
+
+#use of break statement in loops
+i=1    
+while i<=5:
+    print(i)
+    break
+
+#printing numbers till 4 using for loops
+for i in range(1,5):#5 is written as it exclude from range
+    print(i)
+    
+#printing all even number till 10       
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+
+#efficient way of finding even numbers
+for i in range(2,11,2):
+    
+    print(i)
+    
